f8ebfd7623159d0e8bf8062424baf133190eaf58
peterosz <itharius@gmail.com>
2017-10-24 21:06:32 +0200

    Person class with features added

---
+++ CreateClass/CreateClass/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CreateClass
+{
+
+    public class Person
+    {
+        public enum Genders { Male, Female };
+
+        public string Name { get; set; }
+        public int BirthDate { get; set; }
+        public Genders Gender { get; set; }
+
+        public Person() { }
+
+        public override string ToString()
+        {
+            return "\n Name: " + Name +
+                   "\n Date of Birth: " + BirthDate +
+                   "\n Gender: " + Gender;
+        }
+    }
+}
--- CreateClass/CreateClass/Program.cs
+++ CreateClass/CreateClass/Program.cs
@@ -10,6 +10,20 @@ namespace CreateClass
     {
         static void Main(string[] args)
         {
+            Person person1 = new Person();
+
+            person1.Name = "Sanya";
+            person1.BirthDate = 1988;
+            person1.Gender = Person.Genders.Male;
+
+            Person person2 = new Person();
+            person2.Name = "Mariska";
+            person2.BirthDate = 1999;
+            person2.Gender = Person.Genders.Female;
+
+
+            Console.WriteLine(person1.ToString());
+            Console.WriteLine(person2.ToString());
         }
     }
 }
